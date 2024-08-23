b68dedbdc0172677d879fd778208f3da5ea3b10c
Pranshul <pranshul.devmain@gmail.com>
2024-08-23 22:14:12 +0530

    Update apiKeysRotation.js

--- app/src/main/assets/app/apiKeysRotation.js
+++ app/src/main/assets/app/apiKeysRotation.js
@@ -1,40 +1,25 @@
 const apiKeysDaily = [
-    'd94bcd435b62a031771c35633f9f310a',
-    '3b833dcd6aac36124a8a30e182224427',
-    'dd1571a8ad3fd44555e8a5d66db01929',
-    '82005d27a116c2880c8f0fcb866998a0',
+// Keys here
 ];
 
 
 const apiKeysAlerts = [
-    'd94bcd435b62a031771c35633f9f310a',
-    '3b833dcd6aac36124a8a30e182224427',
-    'dd1571a8ad3fd44555e8a5d66db01929',
-    '82005d27a116c2880c8f0fcb866998a0',
+// Keys here
 ];
 
 const apiKeysCityName = [
-    '7147cfac7299479da122684c73d9b80a',
-    '09713ffe266c4dfa99b98157dfd148f9',
-    '66f9ad95f6704259a376949eaa659e62'
+// Keys here
 ];
 
 const apiKeysGeo = [
-    '7147cfac7299479da122684c73d9b80a',
-    '09713ffe266c4dfa99b98157dfd148f9',
-    '66f9ad95f6704259a376949eaa659e62'
+// Keys here
 ];
 
 const apiKeysVisual = [
-    'VGU8JQ9FQKP86FNHZRJEALTLH',
-    'XX6JNDK3D97HKE93N9J75BTXU',
-    '4KMES28T2K3PXBCXTNYZCZFW8'
+// Keys here
   ];
 
 
   const astronomyApiKeys = [
-    '63a7210d2b104646a1099d5ba223d221',
-    '4c502232671d48699afaf3b2741db4a4',
-    '3dba64be2889483880c8534a81b5287e',
-    'a44390988cdb4d66a2e73d952cea14de'
+// Keys here
   ];
