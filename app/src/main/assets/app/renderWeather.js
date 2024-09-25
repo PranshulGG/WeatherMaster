33049709ec5955d2ffeb610fd2149772a1b56eb1
Pranshul <pranshul.devmain@gmail.com>
2024-09-25 09:03:11 +0530

    Updated API url

--- app/src/main/assets/app/renderWeather.js
+++ app/src/main/assets/app/renderWeather.js
@@ -910,7 +910,7 @@ function UvIndex(latitude, longitude) {
 
 
 function MoreDetails(latSum, lonSum) {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=ef2cb48d90984d828a8140518240209&q=${latSum},${lonSum}`)
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=KEY&q=${latSum},${lonSum}`)
         .then(response => response.json())
         .then(data => {
 
@@ -1052,7 +1052,7 @@ function MoreDetails(latSum, lonSum) {
 }
 
 function astronomyData(latSum, lonSum) {
-    fetch(`https://api.weatherapi.com/v1/astronomy.json?key=ef2cb48d90984d828a8140518240209&q=${latSum},${lonSum}`)
+    fetch(`https://api.weatherapi.com/v1/astronomy.json?key=KEY&q=${latSum},${lonSum}`)
         .then(response => response.json())
         .then(data => {
 
