2ef06bc53b81845caa6f2c8c0be2c632a894af69
Pranshul <pranshul.devmain@gmail.com>
2024-11-27 22:02:56 +0530

    added widgets and widget data

---
+++ app/src/main/assets/modules/widgets.js
@@ -0,0 +1,603 @@
+function createWidget(
+  condition,
+  locationWeather,
+  mainTemp,
+  iconData,
+  highLow,
+  hour_0_temp,
+  hour_0_icon,
+  hour_0_time,
+  hour_1_temp,
+  hour_1_icon,
+  hour_1_time,
+  hour_2_temp,
+  hour_2_icon,
+  hour_2_time,
+  hour_3_temp,
+  hour_3_icon,
+  hour_3_time
+) {
+
+  UpdateNotificationInterface.updateNotification(  condition,
+    locationWeather,
+    mainTemp,
+    iconData,
+    highLow,
+    hour_0_temp,
+    hour_0_icon,
+    hour_0_time,
+    hour_1_temp,
+    hour_1_icon,
+    hour_1_time,
+    hour_2_temp,
+    hour_2_icon,
+    hour_2_time,
+    hour_3_temp,
+    hour_3_icon,
+    hour_3_time)
+}
+
+// widget data
+
+function createWidgetData() {
+  const SelectedTempUnit = localStorage.getItem('SelectedTempUnit');
+  const timeFormat = localStorage.getItem('selectedTimeMode');
+  const selectedProvider = localStorage.getItem('selectedMainWeatherProvider')
+
+  const currentLocationData = localStorage.getItem("CurrentLocationName");
+
+  let weatherData;
+
+  if (
+    selectedProvider === "Met norway" ||
+    (localStorage.getItem("ApiForAccu") && selectedProvider === "Accuweather")
+  ) {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataOpenMeteo_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "meteoFrance") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataMeteoFrance_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "dwdGermany") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataDWDGermany_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "noaaUS") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataNOAAUS_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "ecmwf") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataECMWF_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "ukMetOffice") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataukMetOffice_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "jmaJapan") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataJMAJapan_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "gemCanada") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDatagemCanada_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "bomAustralia") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDatabomAustralia_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "cmaChina") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDatacmaChina_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "knmiNetherlands") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataknmiNetherlands_${currentLocationData}`)
+    );
+  } else if (selectedProvider === "dmiDenmark") {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDatadmiDenmark_${currentLocationData}`)
+    );
+  } else {
+    weatherData = JSON.parse(
+      localStorage.getItem(`WeatherDataOpenMeteo_${currentLocationData}`)
+    );
+  }
+
+  let CurrentTemperature;
+
+  if (SelectedTempUnit === "fahrenheit") {
+    CurrentTemperature = Math.round(celsiusToFahrenheit(weatherData.current.temperature_2m)) + '°';
+  } else {
+    CurrentTemperature = Math.round(weatherData.current.temperature_2m) + '°';
+  }
+
+  // -----------
+
+  let locationName;
+
+  if (currentLocationData === "CurrentDeviceLocation") {
+    locationName = "Device location";
+  } else {
+    locationName = currentLocationData;
+  }
+
+  // --------
+
+  let conditionMain;
+
+  conditionMain = getWeatherLabelWidgetLang(weatherData.current.weather_code, localStorage.getItem('AppLanguageCode'));
+
+  // ----------
+
+  let conditionMainIcon;
+
+  conditionMainIcon = GetWeatherIconWidget(weatherData.current.weather_code, weatherData.current.is_day)
+
+  // ------
+
+  let TempMinCurrent
+  let TempMaxCurrent
+  let highLowTemp;
+
+  if (SelectedTempUnit === 'fahrenheit') {
+      TempMinCurrent = Math.round(celsiusToFahrenheit(weatherData.daily.temperature_2m_min[0]))
+  } else {
+      TempMinCurrent = Math.round(weatherData.daily.temperature_2m_min[0])
+  }
+
+  if (SelectedTempUnit === 'fahrenheit') {
+      TempMaxCurrent = Math.round(celsiusToFahrenheit(weatherData.daily.temperature_2m_max[0]))
+  } else {
+      TempMaxCurrent = Math.round(weatherData.daily.temperature_2m_max[0])
+  }
+
+  highLowTemp = `High: ${TempMaxCurrent}° Low: ${TempMinCurrent}°`
+
+
+// hour data
+
+  let hour0Temp
+  let hour0Icon
+  let hour0Time
+
+  let hour1Temp
+  let hour1Icon
+  let hour1Time
+
+  let hour2Temp
+  let hour2Icon
+  let hour2Time
+
+  let hour3Temp
+  let hour3Icon
+  let hour3Time
+
+weatherData.hourly.time.forEach((time, index) => {
+
+    const forecastTime = new Date(time).getTime();
+
+    let hours;
+    let period;
+
+    if (timeFormat === '24 hour') {
+        hours = new Date(time).getHours().toString().padStart(2, '0') + ':';
+        period = new Date(time).getMinutes().toString().padStart(2, '0');
+    } else {
+        let hour = new Date(time).getHours();
+        period = hour >= 12 ? "PM" : "AM";
+        hour = hour % 12 || 12;
+        hours = `${hour}`;
+    }
+
+    const sunriseTimes = weatherData.daily.sunrise.map(time => new Date(time).getTime());
+    const sunsetTimes = weatherData.daily.sunset.map(time => new Date(time).getTime());
+
+    let dayIndex = -1;
+    for (let i = 0; i < sunriseTimes.length; i++) {
+        if (forecastTime >= sunriseTimes[i] && forecastTime < (sunriseTimes[i + 1] || Infinity)) {
+            dayIndex = i;
+            break;
+        }
+    }
+
+    const HourWeatherCodeHour0 = weatherData.hourly.weather_code[0]
+
+    let iconHour0;
+    if (dayIndex !== -1 && forecastTime >= sunriseTimes[dayIndex] && forecastTime < sunsetTimes[dayIndex]) {
+      iconHour0 = GetWeatherIconDayWidget(HourWeatherCodeHour0);
+    } else {
+      iconHour0 = GetWeatherIconNightWidget(HourWeatherCodeHour0);
+    }
+
+    const HourWeatherCodeHour1 = weatherData.hourly.weather_code[1]
+
+    let iconHour1;
+    if (dayIndex !== -1 && forecastTime >= sunriseTimes[dayIndex] && forecastTime < sunsetTimes[dayIndex]) {
+      iconHour1 = GetWeatherIconDayWidget(HourWeatherCodeHour1);
+    } else {
+      iconHour1 = GetWeatherIconNightWidget(HourWeatherCodeHour1);
+    }
+
+    const HourWeatherCodeHour2 = weatherData.hourly.weather_code[2]
+
+    let iconHour2;
+    if (dayIndex !== -1 && forecastTime >= sunriseTimes[dayIndex] && forecastTime < sunsetTimes[dayIndex]) {
+      iconHour2 = GetWeatherIconDayWidget(HourWeatherCodeHour2);
+    } else {
+      iconHour2 = GetWeatherIconNightWidget(HourWeatherCodeHour2);
+    }
+
+    const HourWeatherCodeHour3 = weatherData.hourly.weather_code[3]
+
+    let iconHour3;
+    if (dayIndex !== -1 && forecastTime >= sunriseTimes[dayIndex] && forecastTime < sunsetTimes[dayIndex]) {
+      iconHour3 = GetWeatherIconDayWidget(HourWeatherCodeHour3);
+    } else {
+      iconHour3 = GetWeatherIconNightWidget(HourWeatherCodeHour3);
+    }
+
+    let HourTemperatureHour0;
+
+    if (SelectedTempUnit === 'fahrenheit') {
+      HourTemperatureHour0 = Math.round(celsiusToFahrenheit(weatherData.hourly.temperature_2m[0])) + '°';
+
+    } else {
+      HourTemperatureHour0 = Math.round(weatherData.hourly.temperature_2m[0]) + '°';
+
+    }
+
+    let HourTemperatureHour1;
+
+    if (SelectedTempUnit === 'fahrenheit') {
+      HourTemperatureHour1 = Math.round(celsiusToFahrenheit(weatherData.hourly.temperature_2m[1])) + '°';
+
+    } else {
+      HourTemperatureHour1 = Math.round(weatherData.hourly.temperature_2m[1]) + '°';
+
+    }
+
+    let HourTemperatureHour2;
+
+    if (SelectedTempUnit === 'fahrenheit') {
+      HourTemperatureHour2 = Math.round(celsiusToFahrenheit(weatherData.hourly.temperature_2m[2])) + '°';
+
+    } else {
+      HourTemperatureHour2 = Math.round(weatherData.hourly.temperature_2m[2]) + '°';
+
+    }
+
+    let HourTemperatureHour3;
+
+    if (SelectedTempUnit === 'fahrenheit') {
+      HourTemperatureHour3 = Math.round(celsiusToFahrenheit(weatherData.hourly.temperature_2m[3])) + '°';
+
+    } else {
+      HourTemperatureHour3 = Math.round(weatherData.hourly.temperature_2m[3]) + '°';
+
+    }
+
+
+    const timeString = `${hours}${period}`;
+    if (index === 0) {
+    hour0Icon = iconHour0;
+    hour0Temp = HourTemperatureHour0;
+    hour0Time = timeString;
+
+    } else if (index === 1) {
+    hour1Icon = iconHour1;
+    hour1Temp = HourTemperatureHour1;
+    hour1Time = timeString;
+    } else if (index === 2) {
+    hour2Icon = iconHour2;
+    hour2Temp = HourTemperatureHour2;
+    hour2Time = timeString;
+    } else if (index === 3) {
+    hour3Icon = iconHour3;
+    hour3Temp = HourTemperatureHour3;
+    hour3Time = timeString;
+    }
+
+  })
+
+  createWidget(conditionMain, locationName, CurrentTemperature, conditionMainIcon, highLowTemp, hour0Temp, hour0Icon, hour0Time, hour1Temp, hour1Icon, hour1Time, hour2Temp, hour2Icon, hour2Time, hour3Temp, hour3Icon, hour3Time)
+}
+
+function GetWeatherIconWidget(iconCode, isDay) {
+  if (isDay === 1) {
+    if (iconCode === 0) {
+      return "sunny";
+    } else if (iconCode === 1) {
+      return "mostly_sunny";
+    } else if (iconCode === 2) {
+      return "partly_cloudy";
+    } else if (iconCode === 3) {
+      return "cloudy";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "haze_fog_dust_smoke";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "wintry_mix_rain_snow";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "showers_rain";
+    } else if (iconCode === 65) {
+      return "heavy_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "sleet_hail";
+    } else if (iconCode === 71) {
+      return "snow_showers_snow";
+    } else if (iconCode === 73) {
+      return "snow_showers_snow";
+    } else if (iconCode === 75) {
+      return "heavy_snow";
+    } else if (iconCode === 77) {
+      return "flurries";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "showers_rain";
+    } else if (iconCode === 82) {
+      return "heavy_rain";
+    } else if (iconCode === 85) {
+      return "snow_showers_snow";
+    } else if (iconCode === 86) {
+      return "heavy_snow";
+    } else if (iconCode === 95) {
+      return "isolated_scattered_tstorms_day";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_tstorms";
+    }
+  } else {
+    if (iconCode === 0) {
+      return "clear_night";
+    } else if (iconCode === 1) {
+      return "mostly_clear_night";
+    } else if (iconCode === 2) {
+      return "partly_cloudy_night";
+    } else if (iconCode === 3) {
+      return "cloudy";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "haze_fog_dust_smoke";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "wintry_mix_rain_snow";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "showers_rain";
+    } else if (iconCode === 65) {
+      return "heavy_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "sleet_hail";
+    } else if (iconCode === 71) {
+      return "snow_showers_snow";
+    } else if (iconCode === 73) {
+      return "snow_showers_snow";
+    } else if (iconCode === 75) {
+      return "heavy_snow";
+    } else if (iconCode === 77) {
+      return "flurries";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "showers_rain";
+    } else if (iconCode === 82) {
+      return "heavy_rain";
+    } else if (iconCode === 85) {
+      return "snow_showers_snow";
+    } else if (iconCode === 86) {
+      return "heavy_snow";
+    } else if (iconCode === 95) {
+      return "isolated_scattered_tstorms_night";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_tstorms";
+    }
+  }
+
+  return iconCode;
+}
+
+// labels
+
+function GetWeatherLabelWidget(iconCode, isDay) {
+  if (isDay === 1) {
+    if (iconCode === 0) {
+      return "clear_sky";
+    } else if (iconCode === 1) {
+      return "mostly_sunny";
+    } else if (iconCode === 2) {
+      return "partly_cloudy";
+    } else if (iconCode === 3) {
+      return "overcast";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "fog";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "freezing_drizzle";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "moderate_rain";
+    } else if (iconCode === 65) {
+      return "heavy_intensity_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "freezing_rain";
+    } else if (iconCode === 71) {
+      return "slight_snow";
+    } else if (iconCode === 73) {
+      return "moderate_snow";
+    } else if (iconCode === 75) {
+      return "heavy_intensity_snow";
+    } else if (iconCode === 77) {
+      return "snow_grains";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "rain_showers";
+    } else if (iconCode === 82) {
+      return "heavy_rain_showers";
+    } else if (iconCode === 85) {
+      return "slight_snow_showers";
+    } else if (iconCode === 86) {
+      return "heavy_snow_showers";
+    } else if (iconCode === 95) {
+      return "thunderstorm";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_thunderstorm";
+    }
+  } else {
+    if (iconCode === 0) {
+      return "clear_sky";
+    } else if (iconCode === 1) {
+      return "mostly_clear";
+    } else if (iconCode === 2) {
+      return "partly_cloudy";
+    } else if (iconCode === 3) {
+      return "overcast";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "fog";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "freezing_drizzle";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "moderate_rain";
+    } else if (iconCode === 65) {
+      return "heavy_intensity_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "freezing_rain";
+    } else if (iconCode === 71) {
+      return "slight_snow";
+    } else if (iconCode === 73) {
+      return "moderate_snow";
+    } else if (iconCode === 75) {
+      return "heavy_intensity_snow";
+    } else if (iconCode === 77) {
+      return "snow_grains";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "rain_showers";
+    } else if (iconCode === 82) {
+      return "heavy_rain_showers";
+    } else if (iconCode === 85) {
+      return "slight_snow_showers";
+    } else if (iconCode === 86) {
+      return "heavy_snow_showers";
+    } else if (iconCode === 95) {
+      return "thunderstorm";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_thunderstorm";
+    }
+  }
+
+  return "";
+}
+
+function getWeatherLabelWidgetLang(iconCode, isDay, langCode) {
+  const translationKey = GetWeatherLabelWidget(iconCode, isDay);
+
+  const translatedLabel = getTranslationByLang(langCode, translationKey);
+
+  return translatedLabel || "Unknown weather";
+}
+
+// day icons
+
+
+
+function GetWeatherIconDayWidget(iconCode) {
+
+
+    if (iconCode === 0) {
+      return "sunny";
+    } else if (iconCode === 1) {
+      return "mostly_sunny";
+    } else if (iconCode === 2) {
+      return "partly_cloudy";
+    } else if (iconCode === 3) {
+      return "cloudy";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "haze_fog_dust_smoke";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "wintry_mix_rain_snow";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "showers_rain";
+    } else if (iconCode === 65) {
+      return "heavy_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "sleet_hail";
+    } else if (iconCode === 71) {
+      return "snow_showers_snow";
+    } else if (iconCode === 73) {
+      return "snow_showers_snow";
+    } else if (iconCode === 75) {
+      return "heavy_snow";
+    } else if (iconCode === 77) {
+      return "flurries";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "showers_rain";
+    } else if (iconCode === 82) {
+      return "heavy_rain";
+    } else if (iconCode === 85) {
+      return "snow_showers_snow";
+    } else if (iconCode === 86) {
+      return "heavy_snow";
+    } else if (iconCode === 95) {
+      return "isolated_scattered_tstorms_day";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_tstorms";
+    }
+
+
+return iconCodeDay
+}
+
+// night icons
+
+function GetWeatherIconNightWidget(iconCode) {
+
+
+    if (iconCode === 0) {
+      return "clear_night";
+    } else if (iconCode === 1) {
+      return "mostly_clear_night";
+    } else if (iconCode === 2) {
+      return "partly_cloudy_night";
+    } else if (iconCode === 3) {
+      return "cloudy";
+    } else if (iconCode === 45 || iconCode === 48) {
+      return "haze_fog_dust_smoke";
+    } else if (iconCode === 51 || iconCode === 53 || iconCode === 55) {
+      return "drizzle";
+    } else if (iconCode === 56 || iconCode === 57) {
+      return "wintry_mix_rain_snow";
+    } else if (iconCode === 61 || iconCode === 63) {
+      return "showers_rain";
+    } else if (iconCode === 65) {
+      return "heavy_rain";
+    } else if (iconCode === 66 || iconCode === 67) {
+      return "sleet_hail";
+    } else if (iconCode === 71) {
+      return "snow_showers_snow";
+    } else if (iconCode === 73) {
+      return "snow_showers_snow";
+    } else if (iconCode === 75) {
+      return "heavy_snow";
+    } else if (iconCode === 77) {
+      return "flurries";
+    } else if (iconCode === 80 || iconCode === 81) {
+      return "showers_rain";
+    } else if (iconCode === 82) {
+      return "heavy_rain";
+    } else if (iconCode === 85) {
+      return "snow_showers_snow";
+    } else if (iconCode === 86) {
+      return "heavy_snow";
+    } else if (iconCode === 95) {
+      return "isolated_scattered_tstorms_night";
+    } else if (iconCode === 96 || iconCode === 99) {
+      return "strong_tstorms";
+    }
+
+      return iconCodeNight
+}
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+  createWidgetData()
+})
---
+++ app/src/main/java/com/example/weathermaster/WeatherWidgetProvider.java
@@ -0,0 +1,157 @@
+package com.example.weathermaster;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class WeatherWidgetProvider extends AppWidgetProvider {
+
+    private static final String PREFS_NAME = "WeatherWidgetPrefs";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        // Retrieve persisted data
+        String condition = prefs.getString("condition", "--");
+        String locationWeather = prefs.getString("locationWeather", "--");
+        String mainTemp = prefs.getString("mainTemp", "--°");
+        String iconData = prefs.getString("iconData", "cloudy");
+        String highLow = prefs.getString("highLow", "--");
+
+        String hour_0_temp = prefs.getString("hour_0_temp", "--");
+        String hour_0_icon = prefs.getString("hour_0_icon", "cloudy");
+        String hour_0_time = prefs.getString("hour_0_time", "--");
+
+        String hour_1_temp = prefs.getString("hour_1_temp", "--");
+        String hour_1_icon = prefs.getString("hour_1_icon", "cloudy");
+        String hour_1_time = prefs.getString("hour_1_time", "--");
+
+        String hour_2_temp = prefs.getString("hour_2_temp", "--");
+        String hour_2_icon = prefs.getString("hour_2_icon", "cloudy");
+        String hour_2_time = prefs.getString("hour_2_time", "--");
+
+        String hour_3_temp = prefs.getString("hour_3_temp", "--");
+        String hour_3_icon = prefs.getString("hour_3_icon", "cloudy");
+        String hour_3_time = prefs.getString("hour_3_time", "--");
+
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_weather);
+
+            // Set widget data
+            int iconResId = context.getResources().getIdentifier(iconData, "drawable", context.getPackageName());
+            int iconResIdHour0 = context.getResources().getIdentifier(hour_0_icon, "drawable", context.getPackageName());
+            int iconResIdHour1 = context.getResources().getIdentifier(hour_1_icon, "drawable", context.getPackageName());
+            int iconResIdHour2 = context.getResources().getIdentifier(hour_2_icon, "drawable", context.getPackageName());
+            int iconResIdHour3 = context.getResources().getIdentifier(hour_3_icon, "drawable", context.getPackageName());
+
+            views.setTextViewText(R.id.weather_condition, condition);
+            views.setImageViewResource(R.id.weather_icon, iconResId);
+            views.setTextViewText(R.id.location_name, locationWeather);
+            views.setTextViewText(R.id.temp_text, mainTemp);
+            views.setTextViewText(R.id.high_temp_text, highLow);
+
+            views.setTextViewText(R.id.hour_0_temp, hour_0_temp);
+            views.setImageViewResource(R.id.weather_icon_hour0, iconResIdHour0);
+            views.setTextViewText(R.id.hour_0_time, hour_0_time);
+
+            views.setTextViewText(R.id.hour_1_temp, hour_1_temp);
+            views.setImageViewResource(R.id.weather_icon_hour1, iconResIdHour1);
+            views.setTextViewText(R.id.hour_1_time, hour_1_time);
+
+            views.setTextViewText(R.id.hour_2_temp, hour_2_temp);
+            views.setImageViewResource(R.id.weather_icon_hour2, iconResIdHour2);
+            views.setTextViewText(R.id.hour_2_time, hour_2_time);
+
+            views.setTextViewText(R.id.hour_3_temp, hour_3_temp);
+            views.setImageViewResource(R.id.weather_icon_hour3, iconResIdHour3);
+            views.setTextViewText(R.id.hour_3_time, hour_3_time);
+
+            // Add click listener to open MainActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    public static void updateWeatherWidget(Context context, String condition, String locationWeather, String mainTemp, String iconData, String highLow,
+                                           String hour_0_temp, String hour_0_icon, String hour_0_time,
+                                           String hour_1_temp, String hour_1_icon, String hour_1_time,
+                                           String hour_2_temp, String hour_2_icon, String hour_2_time,
+                                           String hour_3_temp, String hour_3_icon, String hour_3_time) {
+
+        // Save data to SharedPreferences
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("condition", condition);
+        editor.putString("locationWeather", locationWeather);
+        editor.putString("mainTemp", mainTemp);
+        editor.putString("iconData", iconData);
+        editor.putString("highLow", highLow);
+
+        editor.putString("hour_0_temp", hour_0_temp);
+        editor.putString("hour_0_icon", hour_0_icon);
+        editor.putString("hour_0_time", hour_0_time);
+
+        editor.putString("hour_1_temp", hour_1_temp);
+        editor.putString("hour_1_icon", hour_1_icon);
+        editor.putString("hour_1_time", hour_1_time);
+
+        editor.putString("hour_2_temp", hour_2_temp);
+        editor.putString("hour_2_icon", hour_2_icon);
+        editor.putString("hour_2_time", hour_2_time);
+
+        editor.putString("hour_3_temp", hour_3_temp);
+        editor.putString("hour_3_icon", hour_3_icon);
+        editor.putString("hour_3_time", hour_3_time);
+        editor.apply();
+
+        // Update widget views
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName widgetComponent = new ComponentName(context, WeatherWidgetProvider.class);
+        int[] widgetIds = appWidgetManager.getAppWidgetIds(widgetComponent);
+
+        for (int widgetId : widgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_weather);
+
+            int iconResId = context.getResources().getIdentifier(iconData, "drawable", context.getPackageName());
+            int iconResIdHour0 = context.getResources().getIdentifier(hour_0_icon, "drawable", context.getPackageName());
+            int iconResIdHour1 = context.getResources().getIdentifier(hour_1_icon, "drawable", context.getPackageName());
+            int iconResIdHour2 = context.getResources().getIdentifier(hour_2_icon, "drawable", context.getPackageName());
+            int iconResIdHour3 = context.getResources().getIdentifier(hour_3_icon, "drawable", context.getPackageName());
+
+            views.setTextViewText(R.id.weather_condition, condition);
+            views.setImageViewResource(R.id.weather_icon, iconResId);
+            views.setTextViewText(R.id.location_name, locationWeather);
+            views.setTextViewText(R.id.temp_text, mainTemp);
+            views.setTextViewText(R.id.high_temp_text, highLow);
+
+            views.setTextViewText(R.id.hour_0_temp, hour_0_temp);
+            views.setImageViewResource(R.id.weather_icon_hour0, iconResIdHour0);
+            views.setTextViewText(R.id.hour_0_time, hour_0_time);
+
+            views.setTextViewText(R.id.hour_1_temp, hour_1_temp);
+            views.setImageViewResource(R.id.weather_icon_hour1, iconResIdHour1);
+            views.setTextViewText(R.id.hour_1_time, hour_1_time);
+
+            views.setTextViewText(R.id.hour_2_temp, hour_2_temp);
+            views.setImageViewResource(R.id.weather_icon_hour2, iconResIdHour2);
+            views.setTextViewText(R.id.hour_2_time, hour_2_time);
+
+            views.setTextViewText(R.id.hour_3_temp, hour_3_temp);
+            views.setImageViewResource(R.id.weather_icon_hour3, iconResIdHour3);
+            views.setTextViewText(R.id.hour_3_time, hour_3_time);
+
+            appWidgetManager.updateAppWidget(widgetId, views);
+        }
+    }
+}
