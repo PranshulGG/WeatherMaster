d32b487213bfe3530326f31ba3f8ab2f4a167b14
Pranshul <pranshul.devmain@gmail.com>
2024-11-29 18:25:03 +0530

    changed main widget provider

--- app/src/main/java/com/example/weathermaster/WeatherWidgetProvider.java
+++ app/src/main/java/com/example/weathermaster/WeatherWidgetProvider.java
@@ -7,7 +7,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 public class WeatherWidgetProvider extends AppWidgetProvider {
