1a6133ee4bbba39f09b6edf2fab07354f8d12694
Pranshul <pranshul.devmain@gmail.com>
2024-12-04 12:54:15 +0530

    added contributors page

---
+++ app/src/main/java/com/example/weathermaster/contributorsPage.java
@@ -0,0 +1,269 @@
+package com.example.weathermaster;
+
+
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class contributorsPage extends AppCompatActivity {
+
+    private WebView webview;
+
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        setContentView(R.layout.activity_main);
+
+
+
+        // Webview stuff
+        webview = findViewById(R.id.webView);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setAllowContentAccess(true);
+        webview.setVerticalScrollBarEnabled(true);
+        webview.setHorizontalScrollBarEnabled(false);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.setWebViewClient(new WebViewClientDemo());
+        AndroidInterface androidInterface = new AndroidInterface(this);
+        webview.addJavascriptInterface(androidInterface, "AndroidInterface");
+        webview.setBackgroundColor(getResources().getColor(R.color.yellowBg));
+        webview.addJavascriptInterface(new ShowSnackInterface(this), "ShowSnackMessage");
+        webSettings.setTextZoom(100);
+        webSettings.setAllowFileAccess(true);
+        webview.loadUrl("file:///android_asset/pages/contributors.html");
+
+    }
+
+
+    public class ShowSnackInterface {
+        private final Context mContext;
+
+        public ShowSnackInterface(Context context) {
+            this.mContext = context;
+        }
+
+        @JavascriptInterface
+        public void ShowSnack(final String text, final String time) {
+            int duration = Snackbar.LENGTH_SHORT;
+            if ("long".equals(time)) {
+                duration = Snackbar.LENGTH_LONG;
+            } else if ("short".equals(time)){
+                duration = Snackbar.LENGTH_SHORT;
+            }
+
+
+            Snackbar snackbar = Snackbar.make(((Activity) mContext).findViewById(android.R.id.content), text, duration);
+
+            View snackbarView = snackbar.getView();
+
+
+            snackbarView.setBackgroundResource(R.drawable.snackbar_background);
+
+
+            snackbar.setTextColor(ContextCompat.getColor(mContext, R.color.snackbar_text));
+
+
+            ViewGroup.LayoutParams params = snackbar.getView().getLayoutParams();
+            if (params instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginParams = (ViewGroup.MarginLayoutParams) params;
+                marginParams.bottomMargin = 34;
+                marginParams.leftMargin = 26;
+                marginParams.rightMargin = 26;
+                snackbar.getView().setLayoutParams(marginParams);
+            }
+
+
+            snackbar.show();
+        }
+    }
+
+
+    public class AndroidInterface {
+        private contributorsPage aActivity;
+
+        AndroidInterface(contributorsPage activity) {
+            aActivity = activity;
+        }
+
+        @JavascriptInterface
+        public void updateStatusBarColor(final String color) {
+            aActivity.runOnUiThread(new Runnable() {
+                @SuppressLint("ResourceType")
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void run() {
+                    int statusBarColor;
+                    int navigationBarColor;
+                    int systemUiVisibilityFlags = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+
+
+                    if(color.equals("Scrolled")){
+                        statusBarColor = 0xFF1e2024;
+                        navigationBarColor = 0xFF121317;
+                        systemUiVisibilityFlags = 0;
+
+                    } else if (color.equals("ScrollFalse")) {
+                        statusBarColor = 0xFF121317;
+                        navigationBarColor = 0xFF121317;
+                        systemUiVisibilityFlags = 0;
+
+                    } else if (color.equals("GoBack")){
+                        back();
+                        return;
+                    } else if (color.equals("OpenTermsConditions")){
+                        openTermsConditions();
+                        return;
+                    } else if (color.equals("OpenPrivacyPolicy")){
+                        openPrivacyPolicy();
+                        return;
+                    } else if (color.equals("OpenLicenses")){
+                        openThirdParty();
+                        return;
+
+                    } else if (color.equals("bluesetDef")) {
+
+                        return;
+                    } else if (color.equals("keepiton")) {
+                        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        return;
+                    } else if (color.equals("keepitoff")) {
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        return;
+                    } else if (color.equals("itsOn")) {
+                        Toast.makeText(aActivity, "Your device will stay awake", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else if (color.equals("ItsOff")) {
+                        Toast.makeText(aActivity, "Your device will go to sleep at the default time", Toast.LENGTH_SHORT).show();
+                        return;
+
+
+                    } else {
+                        Toast.makeText(aActivity, "not found", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+
+                    int currentStatusBarColor = aActivity.getWindow().getStatusBarColor();
+                    int currentNavigationBarColor = aActivity.getWindow().getNavigationBarColor();
+
+                    ObjectAnimator statusBarAnimator = ObjectAnimator.ofObject(
+                            aActivity.getWindow(),
+                            "statusBarColor",
+                            new ArgbEvaluator(),
+                            currentStatusBarColor,
+                            statusBarColor
+                    );
+
+                    statusBarAnimator.setDuration(200);
+                    statusBarAnimator.start();
+
+                    ObjectAnimator navBarAnimator = ObjectAnimator.ofObject(
+                            aActivity.getWindow(),
+                            "navigationBarColor",
+                            new ArgbEvaluator(),
+                            currentNavigationBarColor,
+                            navigationBarColor
+                    );
+
+                    navBarAnimator.setDuration(200);
+                    navBarAnimator.start();
+
+                    aActivity.getWindow().setNavigationBarColor(navigationBarColor);
+
+                    View decorView = aActivity.getWindow().getDecorView();
+                    decorView.setSystemUiVisibility(systemUiVisibilityFlags);
+
+
+                }
+            });
+        }
+
+
+
+        public void openTermsConditions() {
+            Intent intent = new Intent(aActivity, Terms_Conditions.class);
+            aActivity.startActivity(intent);
+        }
+        public void openPrivacyPolicy() {
+            Intent intent = new Intent(aActivity, PrivacyPolicy.class);
+            aActivity.startActivity(intent);
+        }
+        public void openThirdParty() {
+            Intent intent = new Intent(aActivity, ThirdParty.class);
+            aActivity.startActivity(intent);
+        }
+
+
+    }
+
+    public void back() {
+        onBackPressed();
+    }
+
+
+
+    private static class WebViewClientDemo extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (shouldOpenInBrowser(url)) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                view.getContext().startActivity(intent);
+                return true;
+
+            } else {
+                view.loadUrl(url);
+                return true;
+
+
+            }
+
+
+        }
+
+        private boolean shouldOpenInBrowser(String url) {
+            return url.startsWith("https://fonts.google.com/specimen/Outfit?query=outfit") ||
+                    url.startsWith("https://github.com/");
+
+
+        }
+
+
+    }
+}
