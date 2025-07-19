b78a4636beba5438b52ff2126197143c96225500
Pranshul <pranshul.devmain@gmail.com>
2025-07-19 10:31:37 +0530

    removed sensor permission

--- lib/main.dart
+++ lib/main.dart
@@ -442,6 +442,7 @@ class LocationPromptScreen extends StatelessWidget {
                 saved.latitude,
                 saved.longitude,
                 locationName: cacheKey,
+                context: context
               );
 
 
