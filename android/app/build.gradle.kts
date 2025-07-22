f409494a7f15fdc24ede3116146b3483147ceb92
Pranshul <pranshul.devmain@gmail.com>
2025-07-22 20:29:02 +0530

    added env example / upgraded from jdk 11 to 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,13 +21,13 @@ android {
     
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
