a02b1f58ba1a322d1e80b41a51016e123f0d468a
Pranshul <pranshul.devmain@gmail.com>
2025-08-01 20:19:26 +0530

    updated version

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -333,7 +333,7 @@ class CheckUpdateButton extends StatefulWidget {
 }
 
 class _CheckUpdateButtonState extends State<CheckUpdateButton> {
-  final String currentVersion = 'v2.2.4';
+  final String currentVersion = 'v2.2.5';
   final String githubRepo = 'PranshulGG/WeatherMaster';
   bool isChecking = false;
 
