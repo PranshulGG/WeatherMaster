25c6e1b874f2ae6a854b63376a8151632127c9f1
Fuan200 <juandiazfdez1992@gmail.com>
2025-08-05 12:49:43 -0600

    create code formatting file

---
+++ code_formatting.sh
@@ -0,0 +1,11 @@
+# Code formatting
+
+# Base on flutter docs:
+# https://docs.flutter.dev/tools/formatting
+
+# Do file sh executable:
+#      $ chmod +x code_formatting.sh
+# Execute on terminal:
+#      $ ./code_formatting.sh
+
+dart format ./lib
