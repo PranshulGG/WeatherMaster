0c1edcf96914cfab180a7cf11527f3fa045038b5
Pranshul <pranshul.devmain@gmail.com>
2025-08-21 08:19:33 +0530

    added more url for actual internet access

--- lib/utils/is_online.dart
+++ lib/utils/is_online.dart
@@ -4,10 +4,14 @@ import 'package:http/http.dart' as http;
 // Checks for actual internet access
 Future<bool> hasRealInternet() async {
   final testUrls = [
-    'http://connect.rom.miui.com/generate_204', // MIUI (good in China)
-    'http://captive.apple.com', // Apple (globally reachable)
-    'http://example.com', // Very lightweight
-    'http://baidu.com', // Works in China
+    'http://connect.rom.miui.com/generate_204', // MIUI
+    'http://captive.apple.com', // Apple
+    'http://example.com', // Lightweight
+    'http://baidu.com', // China
+    'http://clients3.google.com/generate_204', // Android
+    'http://www.msftconnecttest.com/connecttest.txt', // Windows
+    'http://www.gstatic.com/generate_204', // Google lightweight
+    'https://example.com', // HTTPS fallback
   ];
 
   try {
