723f2b71f56cb7095f2c27aae4269740bc716f0b
PranshulGG <pranshul.devmain@gmail.com>
2026-04-17 17:05:58 +0530

    add system bar styling imports to MainActivity

--- app/src/main/java/com/pranshulgg/weathermaster/MainActivity.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pranshulgg.weathermaster
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.pranshulgg.weathermaster.core.prefs.AppPrefs.initPrefs
 import com.pranshulgg.weathermaster.core.ui.theme.WeatherMasterTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +27,8 @@ class MainActivity : ComponentActivity() {
         initPrefs(this)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContent {
             WeatherMasterApp()
         }
