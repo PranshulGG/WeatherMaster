42ec6351a8ae1af6eb4c72df80c8a08226ebedfa
PranshulGG <pranshul.devmain@gmail.com>
2026-04-17 17:04:45 +0530

    update weather condition constant from CLEAR_DAY to CLEAR_SKY in background gradients

--- app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/BackgroundGradients.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/BackgroundGradients.kt
@@ -5,7 +5,7 @@ import com.pranshulgg.weathermaster.core.model.WeatherConditions
 
 fun backgroundGradients(condition: WeatherConditions): List<Color> {
     return when (condition) {
-        WeatherConditions.CLEAR_DAY -> listOf(Color(0xFF000630), Color(0xFF004a77))
+        WeatherConditions.CLEAR_SKY -> listOf(Color(0xFF000630), Color(0xFF004a77))
         else -> listOf(Color(0xFF000630), Color(0xFF447FB3))
 
     }
