f32513a5a4dd3af12f4415e5caf60317f95bcf02
PranshulGG <pranshul.devmain@gmail.com>
2026-04-17 17:05:35 +0530

    remove FroggySearchBar and replace with MainSearchBar in FroggyCurrentWeatherCard

--- app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/FroggyCurrentWeatherCard.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/FroggyCurrentWeatherCard.kt
@@ -1,12 +1,11 @@
 package com.pranshulgg.weathermaster.feature.main.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,18 +15,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.pranshulgg.weathermaster.R
-import com.pranshulgg.weathermaster.core.ui.components.Gap
-import com.pranshulgg.weathermaster.feature.main.components.FroggySearchBar
-import com.pranshulgg.weathermaster.feature.main.ui.weatherAnimations.RainCanvas
+import com.pranshulgg.weathermaster.feature.main.components.MainSearchBar
 
 @Composable
-fun FroggyCurrentWeatherCard() {
+fun FroggyCurrentWeatherCard(paddingValues: PaddingValues, navController: NavController) {
 
     val colorScheme = MaterialTheme.colorScheme
 
@@ -37,13 +34,9 @@ fun FroggyCurrentWeatherCard() {
             .height(236.dp)
             .padding(top = 8.dp)
     ) {
-//        RainCanvas(canvasHeight = 236.dp, rainDropCount = 20)
 
-        FroggySearchBar(
-            onSearchClick = {},
-            onSettingsClick = {},
-            hintText = "location name hereee"
-        )
+        MainSearchBar(isFroggyLayout = true, paddingValues = paddingValues, navController)
+
         Row(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
