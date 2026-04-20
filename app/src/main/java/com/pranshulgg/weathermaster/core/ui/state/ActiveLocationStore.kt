230afe37ca01b94a3abe78594c36b8bd7e291b4e
PranshulGG <pranshul.devmain@gmail.com>
2026-04-20 20:27:39 +0530

    update package import for Location from core.model to core.model.domain in ActiveLocationStore

--- app/src/main/java/com/pranshulgg/weathermaster/core/ui/state/ActiveLocationStore.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/ui/state/ActiveLocationStore.kt
@@ -1,6 +1,6 @@
 package com.pranshulgg.weathermaster.core.ui.state
 
-import com.pranshulgg.weathermaster.core.model.Location
+import com.pranshulgg.weathermaster.core.model.domain.Location
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
