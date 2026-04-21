bd53ea97a8aa900710b9f75110fdaf3b595c00c1
PranshulGG <pranshul.devmain@gmail.com>
2026-04-21 22:22:32 +0530

    add ShadowElevation object with elevation levels 1 through 5

---
+++ app/src/main/java/com/pranshulgg/weathermaster/core/ui/theme/ShadowElevation.kt
@@ -0,0 +1,11 @@
+package com.pranshulgg.weathermaster.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object ShadowElevation {
+    val level1 = 1.dp
+    val level2 = 3.dp
+    val level3 = 6.dp
+    val level4 = 8.dp
+    val level5 = 12.dp
+}
