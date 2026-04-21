c60ae06773904e8788d3ae5debb641150c6ab564
PranshulGG <pranshul.devmain@gmail.com>
2026-04-21 22:22:43 +0530

    update TimeFormatters to support time zones and add toWeekdayString method

--- app/src/main/java/com/pranshulgg/weathermaster/core/utils/TimeFormatters.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/utils/TimeFormatters.kt
@@ -8,12 +8,20 @@ import java.util.Locale
 
 class TimeFormatters {
 
-    fun to12HourTimeString(millis: Long): String {
+    fun to12HourTimeString(millis: Long, zoneId: String): String {
         val instant = Instant.ofEpochMilli(millis)
         val formatter = DateTimeFormatter.ofPattern("ha", Locale.ENGLISH)
-            .withZone(ZoneId.systemDefault())
+            .withZone(ZoneId.of(zoneId))
 
         return formatter.format(instant)
+    }
+
+    fun toWeekdayString(millis: Long, zoneId: String): String {
+        val instant = Instant.ofEpochMilli(millis)
+        val zonedDateTime = instant.atZone(ZoneId.of(zoneId))
+        val formatter = DateTimeFormatter.ofPattern("EEE", Locale.ENGLISH)
+
+        return formatter.format(zonedDateTime)
     }
 
 }
