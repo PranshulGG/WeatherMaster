58472d00d2cbbfd3e80c1a41bf1c484617d06a5b
PranshulGG <pranshul.devmain@gmail.com>
2026-04-22 21:59:10 +0530

    add formatNumbers utility function to WeatherUtils for localized decimal formatting

--- app/src/main/java/com/pranshulgg/weathermaster/core/utils/WeatherUtils.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/utils/WeatherUtils.kt
@@ -1,6 +1,7 @@
 package com.pranshulgg.weathermaster.core.utils
 
 import com.pranshulgg.weathermaster.core.model.domain.WeatherHourly
+import java.util.Locale
 
 class WeatherUtils {
 
@@ -15,6 +16,10 @@ class WeatherUtils {
 
         return data.drop(startIndex - 1).take(limit)
 
+    }
+
+    fun formatNumbers(locale: Locale = Locale.US, number: Double, decimalPlaces: Int = 0): String {
+        return "%,.${decimalPlaces}f".format(locale, number)
     }
 
 }
