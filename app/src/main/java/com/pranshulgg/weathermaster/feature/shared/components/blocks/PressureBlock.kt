d96c6620ab120717bc3be3992d120a25788ca63b
PranshulGG <pranshul.devmain@gmail.com>
2026-04-25 22:10:06 +0530

    update PressureBlock to use fillMaxSize instead of fixed size in Box layout

--- app/src/main/java/com/pranshulgg/weathermaster/feature/shared/components/blocks/PressureBlock.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/shared/components/blocks/PressureBlock.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -55,7 +56,7 @@ fun PressureBlock(weather: Weather, units: AppWeatherUnits) {
     ) {
         Box(
             Modifier
-                .size(160.dp)
+                .fillMaxSize()
                 .aspectRatio(1f)
         ) {
             Image(
