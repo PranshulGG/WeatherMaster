ca88781d7a9b5bd85abfa3e286cdb064851e27ba
PranshulGG <pranshul.devmain@gmail.com>
2026-04-25 22:10:49 +0530

    add WeatherBlockType enum for humidity, visibility, UV index, pressure, and sun data types

---
+++ app/src/main/java/com/pranshulgg/weathermaster/core/model/WeatherBlockType.kt
@@ -0,0 +1,10 @@
+package com.pranshulgg.weathermaster.core.model
+
+enum class WeatherBlockType {
+    HUMIDITY_BLOCK,
+    VISIBILITY_BLOCK,
+    UV_INDEX_BLOCK,
+    PRESSURE_BLOCK,
+    SUN_BLOCK
+
+}
