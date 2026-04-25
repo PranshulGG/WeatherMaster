2fd6b91f33d25af4f959e19b928f2bee5979be8f
PranshulGG <pranshul.devmain@gmail.com>
2026-04-25 22:11:23 +0530

    convert WeatherUtils to an object and add helper functions for cache validation, relative time formatting, and weather data filtering

--- app/src/main/java/com/pranshulgg/weathermaster/core/utils/WeatherUtils.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/utils/WeatherUtils.kt
@@ -1,10 +1,16 @@
 package com.pranshulgg.weathermaster.core.utils
 
+import com.pranshulgg.weathermaster.core.model.WeatherResultType
+import com.pranshulgg.weathermaster.core.model.domain.Weather
+import com.pranshulgg.weathermaster.core.model.domain.WeatherDaily
 import com.pranshulgg.weathermaster.core.model.domain.WeatherHourly
+import com.pranshulgg.weathermaster.data.local.entity.WeatherWithRelations
+import java.time.Instant
+import java.time.ZoneId
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
-class WeatherUtils {
-
+object WeatherUtils {
     fun filterHourlyDataForDate(
         data: List<WeatherHourly>,
         currentSeconds: Long,
@@ -20,6 +26,82 @@ class WeatherUtils {
 
     fun formatNumbers(locale: Locale = Locale.US, number: Double, decimalPlaces: Int = 0): String {
         return "%,.${decimalPlaces}f".format(locale, number)
+    }
+
+    fun getLastUpdatedTimeString(timeSeconds: Long): String {
+        val milli = timeSeconds * 1000L
+        val ageMillis = System.currentTimeMillis() - milli
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(ageMillis)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(ageMillis)
+        val hours = TimeUnit.MILLISECONDS.toHours(ageMillis)
+        val days = TimeUnit.MILLISECONDS.toDays(ageMillis)
+
+
+        val lastUpdated = when {
+            seconds < 60 -> "Just now"
+            minutes < 60 -> "$minutes ${if (minutes == 1L) "min" else "mins"} ago"
+            hours < 24 -> "$hours ${if (hours == 1L) "hr" else "hrs"} ago"
+            else -> "$days ${if (days == 1L) "day" else "days"} ago"
+        }
+
+        return lastUpdated
+    }
+
+    fun findMatchingDaily(
+        targetTimeSecs: Long,
+        dailyList: List<WeatherDaily>,
+        timezone: String
+    ): WeatherDaily? {
+
+        val targetDate = Instant.ofEpochSecond(targetTimeSecs)
+            .atZone(ZoneId.of(timezone))
+            .toLocalDate()
+
+
+        return dailyList.firstOrNull { daily ->
+            val dailyDate = Instant.ofEpochSecond(daily.time)
+                .atZone(ZoneId.of(timezone))
+                .toLocalDate()
+
+            targetDate == dailyDate
+        }
+
+    }
+
+    fun isCacheSafe(cache: WeatherWithRelations?): Boolean {
+        val isCacheSafe = cache != null &&
+                cache.daily.isNotEmpty() &&
+                cache.hourly.isNotEmpty() &&
+                cache.current != null
+
+        return isCacheSafe
+    }
+
+    fun isWeatherDomainSafe(weather: Weather?): Boolean {
+        val isCacheSafe = weather != null &&
+                weather.daily.isNotEmpty() &&
+                weather.hourly.isNotEmpty()
+        return isCacheSafe
+    }
+
+    fun shouldReturnCache(cache: WeatherWithRelations?, isRefresh: Boolean): WeatherResultType {
+
+        if (!isCacheSafe(cache)) return WeatherResultType.ERROR
+
+        if (cache?.current == null) return WeatherResultType.ERROR
+
+        val cacheMilli = cache.current.lastUpdatedSecs * 1000L
+        val ageMillis = System.currentTimeMillis() - cacheMilli
+        val ageMinutes = TimeUnit.MILLISECONDS.toMinutes(ageMillis)
+
+        val tooEarly = isRefresh && ageMinutes < 15
+        val maxAge = if (isRefresh) 15 else 4500
+
+        if (tooEarly) return WeatherResultType.REFRESH_TOO_EARLY
+
+        val isSafe = ageMinutes < maxAge
+
+        return if (isSafe) WeatherResultType.SUCCESS else WeatherResultType.ERROR
     }
 
 }
