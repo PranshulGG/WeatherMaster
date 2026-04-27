1590dd4796cb8849f338b9aee0cd6f557d106b15
PranshulGG <pranshul.devmain@gmail.com>
2026-04-27 21:50:29 +0530

    update text style and add imports in CurrentWeatherCard.kt

--- app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/CurrentWeatherCard.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/CurrentWeatherCard.kt
@@ -1,5 +1,6 @@
 package com.pranshulgg.weathermaster.feature.main.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -89,7 +91,7 @@ private fun CardRowContent(weather: Weather, units: AppWeatherUnits) {
         Text(
             current.weatherCondition.toLabel(),
             color = colorScheme.onSurface,
-            style = MaterialTheme.typography.bodyLarge
+            style = MaterialTheme.typography.titleMedium
         )
         Text(
             "Feels like: ${feelsLike.roundToInt()}°",
