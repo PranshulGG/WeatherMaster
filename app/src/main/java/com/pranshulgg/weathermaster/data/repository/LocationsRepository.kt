cfc2de9de55776e71cac52f4383a4bf5c6e8f893
PranshulGG <pranshul.devmain@gmail.com>
2026-04-30 22:47:48 +0530

    remove ActiveLocationStore dependency and usage from LocationsRepository

--- app/src/main/java/com/pranshulgg/weathermaster/data/repository/LocationsRepository.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/data/repository/LocationsRepository.kt
@@ -2,19 +2,15 @@ package com.pranshulgg.weathermaster.data.repository
 
 import androidx.room.Transaction
 import com.pranshulgg.weathermaster.core.model.domain.Location
-import com.pranshulgg.weathermaster.core.ui.state.ActiveLocationStore
 import com.pranshulgg.weathermaster.data.local.dao.WeatherLocationDao
+import com.pranshulgg.weathermaster.data.local.mapper.toDomain
 import com.pranshulgg.weathermaster.data.local.mapper.toEntity
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
-import com.pranshulgg.weathermaster.data.local.mapper.toDomain
 
 class LocationsRepository @Inject constructor(
     private val dao: WeatherLocationDao,
-    private val activeLocationStore: ActiveLocationStore
 ) {
 
     fun getLocations(): Flow<List<Location>> {
@@ -33,7 +29,6 @@ class LocationsRepository @Inject constructor(
             updateDefaultLocation(location.id)
         }
 
-        activeLocationStore.set(location)
     }
 
     @Transaction
