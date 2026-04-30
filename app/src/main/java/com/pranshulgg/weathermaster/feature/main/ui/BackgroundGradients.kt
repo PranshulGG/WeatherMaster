731de658b6d078ac3f139c5b08a23a5dc1da56a4
PranshulGG <pranshul.devmain@gmail.com>
2026-04-30 22:47:36 +0530

    implement light and dark theme support for background gradients using a new ColorPalette interface and ColorsLight object

--- app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/BackgroundGradients.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/main/ui/BackgroundGradients.kt
@@ -1,12 +1,14 @@
 package com.pranshulgg.weathermaster.feature.main.ui
 
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -15,18 +17,23 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import com.pranshulgg.weathermaster.core.model.WeatherConditions
 import com.pranshulgg.weathermaster.core.model.domain.Weather
+import com.pranshulgg.weathermaster.core.prefs.LocalAppPrefs
+import com.pranshulgg.weathermaster.core.ui.theme.isThemeDark
 import com.pranshulgg.weathermaster.core.utils.DataSafe
 import com.pranshulgg.weathermaster.core.utils.WeatherUtils
 import com.pranshulgg.weathermaster.feature.main.ui.Background
 import java.time.Instant
 
-private data class Background(
+data class Background(
     val gradient: List<Color>,
     val scrollColor: Color
 )
 
 @Composable
 fun BackgroundGradient(weather: Weather?, isScrolled: Boolean = false) {
+
+    val isDark = isThemeDark()
+
     AnimatedContent(
         targetState = isScrolled,
         transitionSpec = { fadeIn() togetherWith fadeOut() }) { isScrolled ->
@@ -36,7 +43,7 @@ fun BackgroundGradient(weather: Weather?, isScrolled: Boolean = false) {
                     .fillMaxSize()
                     .background(
                         brush = Brush.verticalGradient(
-                            backgroundGradients(weather).gradient,
+                            backgroundGradients(weather, isDark).gradient,
                             startY = 0f,
                             endY = 1000f
                         )
@@ -46,16 +53,14 @@ fun BackgroundGradient(weather: Weather?, isScrolled: Boolean = false) {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(
-                        color = backgroundGradients(weather).scrollColor
-                    )
+                    .background(color = backgroundGradients(weather, isDark).scrollColor)
             )
         }
     }
 
 }
 
-private fun backgroundGradients(weather: Weather?): Background {
+private fun backgroundGradients(weather: Weather?, isDark: Boolean = true): Background {
 
     if (!DataSafe().isWeatherDomainSafe(weather)) return Colors.CLEAR_SKY_DAY
 
@@ -64,7 +69,7 @@ private fun backgroundGradients(weather: Weather?): Background {
     val isDay = day?.let { weather.current.time in day.sunrise..day.sunset } ?: true
 
 
-    val gradient = gradients(isDay, condition ?: WeatherConditions.CLEAR_SKY)
+    val gradient = gradients(isDay, condition ?: WeatherConditions.CLEAR_SKY, isDark)
 
 
     return gradient
@@ -72,71 +77,127 @@ private fun backgroundGradients(weather: Weather?): Background {
 
 private fun gradients(
     isDay: Boolean = true,
-    condition: WeatherConditions
+    condition: WeatherConditions,
+    isDark: Boolean = true
 ): Background {
+
+    val colors = if (isDark) Colors else ColorsLight
+
     return when (condition) {
 
-        WeatherConditions.CLEAR_SKY -> if (isDay) Colors.CLEAR_SKY_DAY else Colors.CLEAR_SKY_NIGHT
+        WeatherConditions.CLEAR_SKY -> if (isDay) colors.CLEAR_SKY_DAY else colors.CLEAR_SKY_NIGHT
 
-        WeatherConditions.MOSTLY_CLEAR, WeatherConditions.PARTLY_CLOUDY -> Colors.MOSTLY_CLEAR_PARTLY_CLOUDY
+        WeatherConditions.MOSTLY_CLEAR, WeatherConditions.PARTLY_CLOUDY -> colors.MOSTLY_CLEAR_PARTLY_CLOUDY
 
-        WeatherConditions.OVERCAST -> Colors.OVERCAST
+        WeatherConditions.OVERCAST -> colors.OVERCAST
 
-        WeatherConditions.SNOW, WeatherConditions.HEAVY_SNOW, WeatherConditions.LIGHT_SNOW -> Colors.SNOW
+        WeatherConditions.SNOW, WeatherConditions.HEAVY_SNOW, WeatherConditions.LIGHT_SNOW -> colors.SNOW
 
-        WeatherConditions.RAIN, WeatherConditions.HEAVY_RAIN, WeatherConditions.LIGHT_RAIN -> Colors.RAIN
+        WeatherConditions.RAIN, WeatherConditions.HEAVY_RAIN, WeatherConditions.LIGHT_RAIN -> colors.RAIN
 
-        WeatherConditions.FOG_HAZE -> Colors.FOG_HAZE
+        WeatherConditions.FOG_HAZE -> colors.FOG_HAZE
 
-        WeatherConditions.THUNDERSTORM -> Colors.THUNDERSTORM
+        WeatherConditions.THUNDERSTORM -> colors.THUNDERSTORM
 
-        else -> Colors.CLEAR_SKY_NIGHT
+        else -> colors.CLEAR_SKY_DAY
 
     }
 
 }
 
+interface ColorPalette {
+    val CLEAR_SKY_DAY: Background
+    val CLEAR_SKY_NIGHT: Background
+    val MOSTLY_CLEAR_PARTLY_CLOUDY: Background
+    val OVERCAST: Background
+    val SNOW: Background
+    val RAIN: Background
+    val FOG_HAZE: Background
+    val THUNDERSTORM: Background
+}
 
-private object Colors {
+object Colors : ColorPalette {
 
-    val CLEAR_SKY_DAY = Background(
+    override val CLEAR_SKY_DAY = Background(
         gradient = listOf(Color(0xFF08579C), Color(0xFF04008e)),
         scrollColor = Color(0xFF04008e)
     )
 
-    val CLEAR_SKY_NIGHT = Background(
+    override val CLEAR_SKY_NIGHT = Background(
         gradient = listOf(Color(0xFF01023D), Color(0xFF162155)),
         scrollColor = Color(0xFF162155)
     )
 
-    val MOSTLY_CLEAR_PARTLY_CLOUDY = Background(
+    override val MOSTLY_CLEAR_PARTLY_CLOUDY = Background(
         gradient = listOf(Color(0xFF404558), Color(0xFF262C3D)),
         scrollColor = Color(0xFF262C3D)
     )
-    val OVERCAST = Background(
+    override val OVERCAST = Background(
         gradient = listOf(Color(0xFF2F2F34), Color(0xFF202537)),
         scrollColor = Color(0xFF202537)
     )
 
-    val SNOW = Background(
+    override val SNOW = Background(
         gradient = listOf(Color(0xFF171717), Color(0xFF202537)),
         scrollColor = Color(0xFF202537)
     )
 
-    val RAIN = Background(
+    override val RAIN = Background(
         gradient = listOf(Color(0xFF242429), Color(0xFF1e2c3a)),
         scrollColor = Color(0xFF1e2c3a)
     )
 
-    val FOG_HAZE = Background(
+    override val FOG_HAZE = Background(
         gradient = listOf(Color(0xFF191209), Color(0xFF352603)),
         scrollColor = Color(0xFF352603)
     )
 
-    val THUNDERSTORM = Background(
+    override val THUNDERSTORM = Background(
         gradient = listOf(Color(0xFF15021a), Color(0xFF4C2858)),
         scrollColor = Color(0xFF4C2858)
     )
 
 }
 
+private object ColorsLight : ColorPalette {
+
+    override val CLEAR_SKY_DAY = Background(
+        gradient = listOf(Color(0xFF9dceff), Color(0xFFcee5ff)),
+        scrollColor = Color(0xFFcee5ff)
+    )
+
+    override val CLEAR_SKY_NIGHT = Background(
+        gradient = listOf(Color(0xFF969eeb), Color(0xFFE4DFFF)),
+        scrollColor = Color(0xFFE4DFFF)
+    )
+
+    override val MOSTLY_CLEAR_PARTLY_CLOUDY = Background(
+        gradient = listOf(Color(0xFF404558), Color(0xFF262C3D)),
+        scrollColor = Color(0xFF262C3D)
+    )
+    override val OVERCAST = Background(
+        gradient = listOf(Color(0xFFacacad), Color(0xFFd5e4f7)),
+        scrollColor = Color(0xFFd5e4f7)
+    )
+
+    override val SNOW = Background(
+        gradient = listOf(Color(0xFF171717), Color(0xFF202537)),
+        scrollColor = Color(0xFF202537)
+    )
+
+    override val RAIN = Background(
+        gradient = listOf(Color(0xFF242429), Color(0xFF1e2c3a)),
+        scrollColor = Color(0xFF1e2c3a)
+    )
+
+    override val FOG_HAZE = Background(
+        gradient = listOf(Color(0xFF191209), Color(0xFF352603)),
+        scrollColor = Color(0xFF352603)
+    )
+
+    override val THUNDERSTORM = Background(
+        gradient = listOf(Color(0xFF15021a), Color(0xFF4C2858)),
+        scrollColor = Color(0xFF4C2858)
+    )
+
+}
