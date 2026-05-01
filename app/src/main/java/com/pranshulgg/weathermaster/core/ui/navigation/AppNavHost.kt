acec438fdb63116b0cf1774f2b80e4cd41b5956b
PranshulGG <pranshul.devmain@gmail.com>
2026-05-01 22:24:17 +0530

    implement nested navigation in AppNavHost, change start destination to appearance screen, and remove LanguageScreen composable and unused imports

--- app/src/main/java/com/pranshulgg/weathermaster/core/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/ui/navigation/AppNavHost.kt
@@ -10,24 +10,19 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MotionScheme
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.zIndex
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import com.pranshulgg.weathermaster.feature.main.MainScreen
 import com.pranshulgg.weathermaster.feature.search.SearchScreen
 import com.pranshulgg.weathermaster.feature.settings.SettingsScreen
 import com.pranshulgg.weathermaster.feature.settings.appearance.AppearanceScreen
-import com.pranshulgg.weathermaster.feature.settings.language.LanguageScreen
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -41,40 +36,39 @@ fun AppNavHost(
     ) {
         NavHost(
             navController = navController,
-            startDestination = NavRoutes.MAIN,
+            startDestination = "root",
             modifier = Modifier.background(MaterialTheme.colorScheme.surfaceContainer),
             enterTransition = { NavTransitions.enter() },
             exitTransition = { NavTransitions.exit() },
             popEnterTransition = { NavTransitions.popEnter() },
             popExitTransition = { NavTransitions.popExit() }
         ) {
-            composable(
-                NavRoutes.MAIN
+            navigation(
+                route = "root",
+                startDestination = NavRoutes.APPEARANCE // DEFAULT MAIN
             ) {
-                MainScreen(navController)
-            }
-            composable(
-                NavRoutes.SEARCH
-            ) {
-                SearchScreen(navController)
-            }
-            composable(
-                NavRoutes.SETTINGS
-            ) {
-                SettingsScreen(navController)
-            }
-            composable(
-                NavRoutes.LANGUAGE
-            ) {
-                LanguageScreen(navController)
-            }
-            composable(
-                NavRoutes.APPEARANCE
-            ) {
-                AppearanceScreen(navController)
+                composable(
+                    NavRoutes.MAIN
+                ) {
+                    MainScreen(navController)
+                }
+                composable(
+                    NavRoutes.SEARCH
+                ) {
+                    SearchScreen(navController)
+                }
+                composable(
+                    NavRoutes.SETTINGS
+                ) {
+                    SettingsScreen(navController)
+                }
+                composable(
+                    NavRoutes.APPEARANCE
+                ) {
+                    AppearanceScreen(navController)
+                }
             }
         }
-
 
         SnackbarHost(
             hostState = snackbarHostState,
@@ -82,9 +76,11 @@ fun AppNavHost(
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
                 .padding(
-                    bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+                    bottom = WindowInsets.navigationBars.asPaddingValues()
+                        .calculateBottomPadding()
                 )
         )
+
     }
 
 }
