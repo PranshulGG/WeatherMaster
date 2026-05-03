4c920d7c407d7347e56822d7b39e3f13d057ebed
PranshulGG <pranshul.devmain@gmail.com>
2026-05-03 17:08:19 +0530

    add GeoNames search and timezone repositories and APIs

---
+++ app/src/main/java/com/pranshulgg/weathermaster/core/network/search/geonames/GeoNamesSearchDto.kt
@@ -0,0 +1,40 @@
+package com.pranshulgg.weathermaster.core.network.search.geonames
+
+import com.google.gson.annotations.SerializedName
+
+data class GeoNamesSearchDto(
+    val geonames: List<GeoNamesSearchItemDto>
+)
+
+data class GeoNamesSearchItemDto(
+    @SerializedName("lat")
+    val latitude: Double,
+
+    @SerializedName("lng")
+    val longitude: Double,
+
+    val name: String,
+    val countryName: String,
+
+    @SerializedName("country_code")
+    val countryCode: String,
+
+    @SerializedName("adminName1")
+    val state: String?
+
+//    val timezone: String
+)
+
+
+data class GeoNamesTimezoneItemDto(
+    val timezoneId: String
+)
+
+data class GeoNamesTimezoneItem(
+    val timezone: String
+)
+
+fun GeoNamesTimezoneItemDto.toDomain(): GeoNamesTimezoneItem {
+    return GeoNamesTimezoneItem(timezone = timezoneId)
+}
+
