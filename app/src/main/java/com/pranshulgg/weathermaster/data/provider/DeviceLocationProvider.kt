d7b59f56995afde8107890de7cf3c3d7cc0232a6
PranshulGG <pranshul.devmain@gmail.com>
2026-05-05 21:50:02 +0530

    add permission check and refactor provider selection in DeviceLocationProvider.kt

--- app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
@@ -2,16 +2,33 @@ package com.pranshulgg.weathermaster.data.provider
 
 import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.util.Log
 import androidx.annotation.RequiresPermission
+import androidx.core.content.ContextCompat
 
 data class DeviceLocation(
     val latitude: Double?,
     val longitude: Double?
 )
 
-@RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
 fun getDeviceLocation(context: Context): DeviceLocation {
+
+    val hasPermission =
+        ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+
+    if (!hasPermission) {
+        return DeviceLocation(null, null)
+    }
+
     val locationManager =
         context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
             ?: return DeviceLocation(null, null)
@@ -20,24 +37,23 @@ fun getDeviceLocation(context: Context): DeviceLocation {
     var lon: Double? = null
 
 
-    val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-    val hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    val providers = listOf(
+        LocationManager.NETWORK_PROVIDER,
+        LocationManager.GPS_PROVIDER
+    )
 
-    val provider = when {
-        hasGps -> LocationManager.GPS_PROVIDER
-        hasNetwork -> LocationManager.NETWORK_PROVIDER
-        else -> null
-    }
-
-    if (provider != null) {
-        val location = locationManager.getLastKnownLocation(provider)
-
-        location?.let {
-            lat = it.latitude
-            lon = it.longitude
+    for (p in providers) {
+        if (locationManager.isProviderEnabled(p)) {
+            val loc = locationManager.getLastKnownLocation(p)
+            if (loc != null) {
+                lat = loc.latitude
+                lon = loc.longitude
+                break
+            }
         }
-
     }
+
+
 
     return DeviceLocation(lat, lon)
 
