2a29057782e3f0e36f5ac0fca8804d00fd95c78f
PranshulGG <pranshul.devmain@gmail.com>
2026-05-06 14:38:08 +0530

    add rememberLocationPermissionLauncher composable to DeviceLocationProvider.kt

--- app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
@@ -2,11 +2,18 @@ package com.pranshulgg.weathermaster.data.provider
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.core.content.ContextCompat
+import com.pranshulgg.weathermaster.feature.intro.toDomain
 
 data class DeviceLocation(
     val latitude: Double?,
@@ -57,4 +64,33 @@ fun getDeviceLocation(context: Context): DeviceLocation {
 
     return DeviceLocation(lat, lon)
 
+}
+
+@Composable
+fun rememberLocationPermissionLauncher(
+    onGranted: () -> Unit,
+    onDenied: () -> Unit
+): () -> Unit {
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val fine = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val coarse = permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+
+        if (fine || coarse) {
+            onGranted()
+        } else {
+            onDenied()
+        }
+    }
+
+    return {
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
 }
