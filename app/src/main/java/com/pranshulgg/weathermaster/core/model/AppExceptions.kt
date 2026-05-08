eee86e2e842a15985d4c340845d000cda94cfcde
PranshulGG <pranshul.devmain@gmail.com>
2026-05-08 21:34:14 +0530

    add toAppException extension function in AppExceptions.kt

--- app/src/main/java/com/pranshulgg/weathermaster/core/model/AppExceptions.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/core/model/AppExceptions.kt
@@ -1,6 +1,11 @@
 package com.pranshulgg.weathermaster.core.model
 
+import coil.network.HttpException
 import com.pranshulgg.weathermaster.R
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import kotlin.coroutines.cancellation.CancellationException
 
 sealed class AppException(message: String? = null) : Exception(message) {
     class Network : AppException()
@@ -16,5 +21,19 @@ fun AppException.toMessageRes(): Int {
         is AppException.CurrentLocationUnavailable -> R.string.current_location_not_found
         is AppException.Server -> R.string.error_server
         is AppException.Unknown -> R.string.error_generic
+    }
+}
+
+fun Throwable.toAppException(): AppException {
+    if (this is CancellationException) throw this
+
+    return when (this) {
+        is UnknownHostException,
+        is SocketTimeoutException,
+        is IOException -> AppException.Network()
+
+        is HttpException -> AppException.Server()
+
+        else -> AppException.Unknown()
     }
 }
