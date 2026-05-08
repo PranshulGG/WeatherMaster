a1710f746be025c87f892a6ca9a389babac55a13
PranshulGG <pranshul.devmain@gmail.com>
2026-05-08 21:34:23 +0530

    add toAppException extension function in AppExceptions.kt

--- app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/data/provider/DeviceLocationProvider.kt
@@ -4,26 +4,62 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.core.content.ContextCompat
+import com.google.gson.annotations.Until
+import com.pranshulgg.weathermaster.core.model.AppException
 import com.pranshulgg.weathermaster.feature.intro.toDomain
+import okio.Timeout
 
 data class DeviceLocation(
     val latitude: Double?,
     val longitude: Double?
 )
 
-fun getDeviceLocation(context: Context): DeviceLocation {
+class GetDeviceLocation {
 
-    val hasPermission =
-        ContextCompat.checkSelfPermission(
+    private var locationManager: LocationManager? = null
+    private var locationListener: LocationListener? = null
+
+    private var timeoutHandler: Handler? = null
+    private var timeoutRunnable: Runnable? = null
+
+    private val timeoutSecs = 10_000L // 10 seconds
+
+    private fun getProvider(lm: LocationManager): String {
+        return when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+                    lm.allProviders.contains(LocationManager.FUSED_PROVIDER) -> LocationManager.FUSED_PROVIDER
+
+            lm.allProviders.contains(LocationManager.NETWORK_PROVIDER) -> LocationManager.NETWORK_PROVIDER
+            lm.allProviders.contains(LocationManager.GPS_PROVIDER) -> LocationManager.GPS_PROVIDER
+            else -> LocationManager.PASSIVE_PROVIDER
+        }
+    }
+
+    fun getDeviceLocation(
+        context: Context,
+        onTimeout: () -> Unit,
+        onResult: (DeviceLocation) -> Unit
+    ) {
+
+
+        val hasPermission = ContextCompat.checkSelfPermission(
             context,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED ||
@@ -32,38 +68,82 @@ fun getDeviceLocation(context: Context): DeviceLocation {
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED
 
-    if (!hasPermission) {
-        return DeviceLocation(null, null)
-    }
 
-    val locationManager =
-        context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
-            ?: return DeviceLocation(null, null)
-
-    var lat: Double? = null
-    var lon: Double? = null
-
-
-    val providers = listOf(
-        LocationManager.NETWORK_PROVIDER,
-        LocationManager.GPS_PROVIDER
-    )
-
-    for (p in providers) {
-        if (locationManager.isProviderEnabled(p)) {
-            val loc = locationManager.getLastKnownLocation(p)
-            if (loc != null) {
-                lat = loc.latitude
-                lon = loc.longitude
-                break
-            }
+        if (!hasPermission) {
+            onResult(DeviceLocation(null, null))
+            return
         }
+
+        val lm = context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
+            ?: run {
+                onResult(DeviceLocation(null, null))
+                return
+            }
+
+        locationManager = lm
+
+        val provider = getProvider(lm)
+
+        if (!lm.isProviderEnabled(provider)) {
+            onResult(DeviceLocation(null, null))
+            return
+        }
+
+
+        getLocation(onLocation = { onResult(it) }, lm, provider, onTimeout)
+    }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun getLocation(
+        onLocation: (DeviceLocation) -> Unit,
+        lm: LocationManager,
+        provider: String,
+        onTimeout: () -> Unit
+    ) {
+
+        val lastKnown = lm.getLastKnownLocation(provider)
+
+        if (lastKnown != null) {
+            timeoutHandler?.removeCallbacks(timeoutRunnable!!)
+            onLocation(DeviceLocation(lastKnown.latitude, lastKnown.longitude))
+            return
+        }
+
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                timeoutHandler?.removeCallbacks(timeoutRunnable!!)
+                onLocation(DeviceLocation(location.latitude, location.longitude))
+                stopUpdates()
+            }
+
+            override fun onProviderEnabled(provider: String) {}
+
+            override fun onProviderDisabled(provider: String) {}
+        }
+
+
+        lm.requestLocationUpdates(
+            provider,
+            5000L,
+            5f,
+            locationListener!!
+        )
+
+        timeoutHandler = Handler(Looper.getMainLooper())
+
+        timeoutRunnable = Runnable {
+            stopUpdates()
+            onTimeout()
+        }
+
+        timeoutHandler?.postDelayed(timeoutRunnable!!, timeoutSecs)
     }
 
 
-
-    return DeviceLocation(lat, lon)
-
+    fun stopUpdates() {
+        locationListener?.let { locationManager?.removeUpdates(it) }
+        locationListener = null
+    }
 }
 
 @Composable
