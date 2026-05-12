466a314b1298cbfb9cfdc936f47bb5e467929f64
PranshulGG <pranshul.devmain@gmail.com>
2026-05-12 20:39:23 +0530

    add CurrentAirQualityEntity Room entity

---
+++ app/src/main/java/com/pranshulgg/weathermaster/data/local/entity/CurrentAirQualityEntity.kt
@@ -0,0 +1,22 @@
+package com.pranshulgg.weathermaster.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "air_quality_current")
+data class CurrentAirQualityEntity(
+
+    @PrimaryKey
+    val locationId: String,
+
+    val usAqi: Int?,
+    val pm10: Double?,
+    val pm25: Double?,
+    val carbonMonoxide: Double?,
+    val nitrogenDioxide: Double?,
+    val sulphurDioxide: Double?,
+    val ozone: Double?,
+    val lastUpdatedSecs: Long
+)
+
