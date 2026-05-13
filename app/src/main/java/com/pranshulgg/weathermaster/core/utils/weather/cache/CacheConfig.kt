a6cfd39c8ea663598bf3be4d8ef318e7e5a25cb3
PranshulGG <pranshul.devmain@gmail.com>
2026-05-13 21:30:08 +0530

    add CacheConfig with weather refresh constants

---
+++ app/src/main/java/com/pranshulgg/weathermaster/core/utils/weather/cache/CacheConfig.kt
@@ -0,0 +1,8 @@
+package com.pranshulgg.weathermaster.core.utils.weather.cache
+
+object CacheConfig {
+
+    const val MANUAL_REFRESH_MINUTES = 15
+    const val AUTO_REFRESH_MAX_MINUTES = 4500 // TODO: DEFAULT 45
+
+}
