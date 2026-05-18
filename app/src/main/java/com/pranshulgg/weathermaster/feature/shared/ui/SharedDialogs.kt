96e837fe10069fe9868a04202762c6b42a7da9da
PranshulGG <pranshul.devmain@gmail.com>
2026-05-18 14:42:20 +0530

    implement WeatherProvidersForLocationDialog in SharedDialogs.kt

--- app/src/main/java/com/pranshulgg/weathermaster/feature/shared/ui/SharedDialogs.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/feature/shared/ui/SharedDialogs.kt
@@ -1,6 +1,17 @@
 package com.pranshulgg.weathermaster.feature.shared.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.pranshulgg.weathermaster.R
+import com.pranshulgg.weathermaster.core.model.sources.WeatherSource
+import com.pranshulgg.weathermaster.core.model.sources.toName
+import com.pranshulgg.weathermaster.core.model.sources.weatherProviderGetForCountry
+import com.pranshulgg.weathermaster.core.ui.components.DialogBasic
+import com.pranshulgg.weathermaster.core.ui.components.RadioRow
 
 object SharedDialogs {
 
@@ -8,9 +19,39 @@ object SharedDialogs {
     @Composable
     fun WeatherProvidersForLocationDialog(
         countryCode: String,
+        show: Boolean,
+        selectedSource: WeatherSource = WeatherSource.OPEN_METEO,
         onSave: () -> Unit,
         onCancel: () -> Unit
     ) {
+
+        val supportedProviders = weatherProviderGetForCountry(countryCode)
+        var selectedProvider by remember(
+            show,
+            selectedSource
+        ) {
+            mutableStateOf(selectedSource)
+        }
+
+        DialogBasic(
+            title = "Sources",
+            onConfirm = {
+                onSave()
+            },
+            onDismiss = onCancel,
+            show = show,
+            confirmText = stringResource(R.string.action_save),
+            dismissText = stringResource(R.string.action_cancel)
+        ) {
+            supportedProviders.forEach { provider ->
+                RadioRow(
+                    label = provider.toName(),
+                    onClick = { selectedProvider = WeatherSource.valueOf(it) },
+                    selected = selectedProvider == provider,
+                    value = provider.toString()
+                )
+            }
+        }
 
     }
 
