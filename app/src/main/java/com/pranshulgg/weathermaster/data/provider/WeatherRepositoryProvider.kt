2ec3320d8a0cfa8450737775704e627e97f5b903
PranshulGG <pranshul.devmain@gmail.com>
2026-05-20 18:34:36 +0530

    add NWS repository to WeatherRepositoryProvider and introduce WeatherDataReconcilerRepository to clean up stale data

--- app/src/main/java/com/pranshulgg/weathermaster/data/provider/WeatherRepositoryProvider.kt
+++ app/src/main/java/com/pranshulgg/weathermaster/data/provider/WeatherRepositoryProvider.kt
@@ -1,17 +1,20 @@
 package com.pranshulgg.weathermaster.data.provider
 
 import com.pranshulgg.weathermaster.core.model.sources.WeatherSource
+import com.pranshulgg.weathermaster.core.network.sources.weather.nws.NwsRepository
 import com.pranshulgg.weathermaster.core.network.sources.weather.openmeteo.OpenMeteoRepository
 import com.pranshulgg.weathermaster.data.repository.WeatherRepository
 import javax.inject.Inject
 
 class WeatherRepositoryProvider @Inject constructor(
-    private val openMeteoRepo: OpenMeteoRepository
+    private val openMeteoRepo: OpenMeteoRepository,
+    private val nwsRepo: NwsRepository
 ) {
 
     fun getRepository(source: WeatherSource): WeatherRepository {
         return when (source) {
             WeatherSource.OPEN_METEO -> openMeteoRepo
+            WeatherSource.NWS -> nwsRepo
         }
     }
 
