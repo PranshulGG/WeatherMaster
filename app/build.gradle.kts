230eae31182ee4982c0b1b22f8bd6a7ce3d9e842
PranshulGG <pranshul.devmain@gmail.com>
2026-05-22 15:42:55 +0530

    bump version to 3.0.0-beta3 and versionCode to 46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.pranshulgg.weathermaster"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "3.0.0-beta2"
+        versionCode = 46
+        versionName = "3.0.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
