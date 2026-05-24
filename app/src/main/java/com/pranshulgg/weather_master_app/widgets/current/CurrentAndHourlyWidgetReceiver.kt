296e59b911410d5b822a3f0b3385d7601034c457
PranshulGG <pranshul.devmain@gmail.com>
2026-05-24 12:37:30 +0530

    add CurrentAndHourlyWidget and UI content using Jetpack Glance

---
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/current/CurrentAndHourlyWidgetReceiver.kt
@@ -0,0 +1,15 @@
+package com.pranshulgg.weather_master_app.widgets.current
+
+import android.content.Context
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import com.pranshulgg.weather_master_app.data.worker.WeatherWorker
+
+class CurrentAndHourlyWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget = CurrentAndHourlyWidget()
+
+    override fun onEnabled(context: Context) {
+        super.onEnabled(context)
+
+        WeatherWorker.startNow(context)
+    }
+}
