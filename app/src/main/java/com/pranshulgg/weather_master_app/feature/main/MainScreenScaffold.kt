ee91e8f8d29ad1275d90a146a7ff775f43702cc6
PranshulGG <pranshul.devmain@gmail.com>
2026-05-24 12:47:05 +0530

    add ExperimentalMaterial3ExpressiveApi opt-in to MainScreenScaffold.kt

--- app/src/main/java/com/pranshulgg/weather_master_app/feature/main/MainScreenScaffold.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/feature/main/MainScreenScaffold.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.LoadingIndicator
@@ -37,6 +38,7 @@ import com.pranshulgg.weather_master_app.feature.main.ui.weatherAnimations.Weath
 import com.pranshulgg.weather_master_app.feature.shared.ui.DailyCard
 import com.pranshulgg.weather_master_app.feature.shared.ui.HourlyCard
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun MainScreenScaffold(
     navController: NavController,
