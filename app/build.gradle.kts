04ae4407ddf36abf893e16e51b55df205e6576df
PranshulGG <pranshul.devmain@gmail.com>
2026-05-25 13:31:20 +0530

    add release signing configuration to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,21 @@ android {
             "\"$geoNamesUserNameKey\""
         )
     }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("../keystore/release.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
+
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
