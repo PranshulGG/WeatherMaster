cd91bd370525bd69ac1d4c4ed031d237a2b2ee01
PranshulGG <pranshul.devmain@gmail.com>
2026-06-10 16:09:07 +0530

    add WeatherSmallWidget and WeatherHorizontalWidget to AndroidManifest and WeatherWidgetUpdater

--- app/src/main/java/com/pranshulgg/weather_master_app/data/worker/widgets/WidgetWeatherUpdater.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/data/worker/widgets/WidgetWeatherUpdater.kt
@@ -8,6 +8,10 @@ import com.pranshulgg.weather_master_app.widgets.WeatherWidgetStateJson
 import com.pranshulgg.weather_master_app.widgets.pill.WidgetPill
 import com.pranshulgg.weather_master_app.widgets.summary.SummaryWidget
 import com.pranshulgg.weather_master_app.widgets.weather.WeatherWidget
+import com.pranshulgg.weather_master_app.widgets.weatherhorizontal.WeatherHorizontalWidget
+import com.pranshulgg.weather_master_app.widgets.weatherhorizontal.ui.WeatherWidgetHorizontal
+import com.pranshulgg.weather_master_app.widgets.weathersmall.WeatherSmallWidget
+import com.pranshulgg.weather_master_app.widgets.weathersmall.ui.WeatherWidgetSmall
 
 class WeatherWidgetUpdater(
     private val context: Context
@@ -16,6 +20,9 @@ class WeatherWidgetUpdater(
     private val widget = WeatherWidget()
     private val pill = WidgetPill()
     private val summary = SummaryWidget()
+    private val widgetSmall = WeatherSmallWidget()
+
+    private val widgetHorizontal = WeatherHorizontalWidget()
 
     suspend fun update(json: String) {
 
@@ -25,6 +32,8 @@ class WeatherWidgetUpdater(
         val widgetIds = manager.getGlanceIds(WeatherWidget::class.java)
         val pillIds = manager.getGlanceIds(WidgetPill::class.java)
         val summaryIds = manager.getGlanceIds(SummaryWidget::class.java)
+        val widgetSmallIds = manager.getGlanceIds(WeatherSmallWidget::class.java)
+        val widgetHorizontalIds = manager.getGlanceIds(WeatherHorizontalWidget::class.java)
 
         widgetIds.forEach {
             updateAppWidgetState(context, WeatherWidgetStateDefinition, it) {
@@ -45,6 +54,20 @@ class WeatherWidgetUpdater(
                 WeatherWidgetStateJson(json)
             }
             summary.update(context, it)
+        }
+
+        widgetSmallIds.forEach {
+            updateAppWidgetState(context, WeatherWidgetStateDefinition, it) {
+                WeatherWidgetStateJson(json)
+            }
+            widgetSmall.update(context, it)
+        }
+
+        widgetHorizontalIds.forEach {
+            updateAppWidgetState(context, WeatherWidgetStateDefinition, it) {
+                WeatherWidgetStateJson(json)
+            }
+            widgetHorizontal.update(context, it)
         }
     }
 }
