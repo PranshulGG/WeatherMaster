a0dbcb9a21a506072894666c60599ac4b35eeadf
PranshulGG <pranshul.devmain@gmail.com>
2026-06-20 15:13:23 +0530

    add size-specific clock layout files and update ViewClock to select layouts based on text size

--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/ui/views/ViewClock.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/ui/views/ViewClock.kt
@@ -4,27 +4,31 @@ import android.content.Context
 import android.util.TypedValue
 import android.widget.RemoteViews
 import androidx.compose.runtime.Composable
-import androidx.glance.LocalContext
 import androidx.glance.appwidget.AndroidRemoteViews
 import com.pranshulgg.weather_master_app.R
 
 
 private fun createClock(
     context: Context,
-    sizeSp: Float
+    size: Float
 ): RemoteViews {
+
+    val layoutId = when (size) {
+        20f -> R.layout.sys_clock_20
+        28f -> R.layout.sys_clock_28
+        36f -> R.layout.sys_clock_36
+        44f -> R.layout.sys_clock_44
+        56f -> R.layout.sys_clock_56
+        72f -> R.layout.sys_clock_72
+        88f -> R.layout.sys_clock_88
+        104f -> R.layout.sys_clock_104
+        else -> R.layout.sys_clock_120
+    }
 
     return RemoteViews(
         context.packageName,
-        R.layout.sys_clock
-    ).apply {
-
-        setTextViewTextSize(
-            R.id.clock,
-            TypedValue.COMPLEX_UNIT_SP,
-            sizeSp
-        )
-    }
+        layoutId
+    )
 }
 
 @Composable
