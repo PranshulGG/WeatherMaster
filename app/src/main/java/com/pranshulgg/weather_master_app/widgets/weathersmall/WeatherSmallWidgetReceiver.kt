4478f7c98a8354680dd439fed4dfc900ba246b95
PranshulGG <pranshul.devmain@gmail.com>
2026-06-20 15:12:41 +0530

    remove small weather widget and WidgetConfig

--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/weathersmall/WeatherSmallWidgetReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.pranshulgg.weather_master_app.widgets.weathersmall
-
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import com.pranshulgg.weather_master_app.widgets.weather.WeatherWidget
-
-
-class WeatherSmallWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget = WeatherSmallWidget()
-}
