9e2dc088d057317b0eaf225ad0fdf7550491d0e4
PranshulGG <pranshul.devmain@gmail.com>
2026-06-20 15:15:16 +0530

    trigger weather worker on widget update and add config to WeatherWidgetStateJson

--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/WeatherWidgetStateJson.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/WeatherWidgetStateJson.kt
@@ -1,8 +1,10 @@
 package com.pranshulgg.weather_master_app.widgets
 
+import com.pranshulgg.weather_master_app.widgets.config.WidgetConfig
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class WeatherWidgetStateJson(
-    val json: String? = null
+    val json: String? = null,
+    val config: WidgetConfig = WidgetConfig()
 )
--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/weather/WeatherWidgetReceiver.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/weather/WeatherWidgetReceiver.kt
@@ -1,9 +1,18 @@
 package com.pranshulgg.weather_master_app.widgets.weather
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import com.pranshulgg.weather_master_app.data.worker.WeatherWorker
 
 class WeatherWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget = WeatherWidget()
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        WeatherWorker.runWorkerOnce(context)
+    }
 }
