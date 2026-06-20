f543509d71c578676901df2d8ca0d4f818d33045
PranshulGG <pranshul.devmain@gmail.com>
2026-06-20 15:17:26 +0530

    trigger weather worker on widget update in SummaryWidgetReceiver and WidgetPillReceiver

--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/pill/WidgetPillReceiver.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/pill/WidgetPillReceiver.kt
@@ -1,5 +1,6 @@
 package com.pranshulgg.weather_master_app.widgets.pill
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import com.pranshulgg.weather_master_app.data.worker.WeatherWorker
@@ -8,4 +9,13 @@ import com.pranshulgg.weather_master_app.widgets.weather.WeatherWidget
 
 class WidgetPillReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget = WidgetPill()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        WeatherWorker.runWorkerOnce(context)
+    }
 }
--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/summary/SummaryWidgetReceiver.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/summary/SummaryWidgetReceiver.kt
@@ -1,5 +1,6 @@
 package com.pranshulgg.weather_master_app.widgets.summary
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import com.pranshulgg.weather_master_app.data.worker.WeatherWorker
@@ -7,4 +8,13 @@ import com.pranshulgg.weather_master_app.data.worker.WeatherWorker
 
 class SummaryWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget = SummaryWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        WeatherWorker.runWorkerOnce(context)
+    }
 }
