2a17909935c449f591188fc10ea29a030ffd7413
PranshulGG <pranshul.devmain@gmail.com>
2026-06-24 14:40:11 +0530

    bump version code and version name to 3.5.1 (54)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.pranshulgg.weather_master_app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 53
-        versionName = "3.5.0"
+        versionCode = 54
+        versionName = "3.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
