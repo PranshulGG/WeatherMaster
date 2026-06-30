c34f113de3c25d0eca6adfc24392a651f48b2c0a
PranshulGG <pranshul.devmain@gmail.com>
2026-06-30 20:43:13 +0530

    Fixed wind speed forecast using the wrong time

--- app/src/main/java/com/pranshulgg/weather_master_app/feature/blocks/screens/wind/WindScreen.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/feature/blocks/screens/wind/WindScreen.kt
@@ -1,13 +1,10 @@
 package com.pranshulgg.weather_master_app.feature.blocks.screens.wind
 
-import android.content.Context
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -16,34 +13,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.pranshulgg.weather_master_app.R
-import com.pranshulgg.weather_master_app.core.model.weather.WindSpeedUnit
-import com.pranshulgg.weather_master_app.core.model.weather.toName
-import com.pranshulgg.weather_master_app.core.model.weather.wind.WindDirection
-import com.pranshulgg.weather_master_app.core.prefs.LocalAppPrefs
 import com.pranshulgg.weather_master_app.core.ui.components.Gap
 import com.pranshulgg.weather_master_app.core.ui.components.LargeTopBarScaffold
 import com.pranshulgg.weather_master_app.core.ui.components.NavigateUpBtn
-import com.pranshulgg.weather_master_app.core.utils.formatters.to12HourTimeString
-import com.pranshulgg.weather_master_app.core.utils.formatters.to24HourTimeString
 import com.pranshulgg.weather_master_app.core.utils.formatters.toDateString
 import com.pranshulgg.weather_master_app.core.utils.weather.forecast.findMatchingHourly
 import com.pranshulgg.weather_master_app.feature.blocks.BlocksScreenViewModel
 import com.pranshulgg.weather_master_app.feature.blocks.components.AboutCard
 import com.pranshulgg.weather_master_app.feature.blocks.components.AboutCardText
-import com.pranshulgg.weather_master_app.feature.blocks.components.MatBarChart
 import com.pranshulgg.weather_master_app.feature.blocks.screens.wind.components.WindHourlyCard
-import kotlin.math.max
-import kotlin.math.roundToInt
 
 
 @Composable
@@ -63,10 +47,11 @@ fun WindScreen(navController: NavController, index: Int = 0, locationId: String)
     val units = uiState.units
     val context = LocalContext.current
     val zoneId = weather.location.timezone
+    val time = if (index != 0) weather.daily[index].time else weather.current.time
 
     val data = findMatchingHourly(
         hourly,
-        weather.daily[index].time,
+        time,
         weather.location.source,
 
         )
