127f86bcd91b198aab8e8ed5505b7cbda62d0400
PranshulGG <pranshul.devmain@gmail.com>
2026-07-12 17:27:00 +0530

    add date and clock variants to WidgetVariant enum

--- app/src/main/java/com/pranshulgg/weather_master_app/widgets/model/WidgetVariant.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/widgets/model/WidgetVariant.kt
@@ -3,5 +3,15 @@ package com.pranshulgg.weather_master_app.widgets.model
 enum class WidgetVariant(val label: String) {
     LARGE("Large"),
     COMPACT("Compact"),
-    SMALL("Small")
+    SMALL("Small"),
+
+
+    // WEATHER 4 - VARIANTS
+
+    DATE("Date"),
+    DATE_PILL("Date Pill"),
+    CLOCK_DATE("Clock & Date"),
+    CLOCK_VERTICAL("Clock Vertical"),
+    CLOCK_HORIZONTAL("Clock Horizontal"),
+    
 }
