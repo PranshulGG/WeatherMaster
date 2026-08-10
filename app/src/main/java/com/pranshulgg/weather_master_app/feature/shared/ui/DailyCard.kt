ad0a96f2909f2bb65d06e7f51a4cb535c272354f
PranshulGG <pranshul.devmain@gmail.com>
2026-08-10 17:34:14 +0530

    date cutting off in German closes #1007

--- app/src/main/java/com/pranshulgg/weather_master_app/feature/shared/ui/DailyCard.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/feature/shared/ui/DailyCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -120,7 +121,7 @@ private fun DailyItem(
     ) {
         Column(
             Modifier
-                .height(210.dp)
+                .heightIn(210.dp)
                 .width(65.dp)
                 .padding(vertical = 24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
