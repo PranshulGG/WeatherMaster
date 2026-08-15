64eb2cac319f3463872effbd9135a393e52d8296
PranshulGG <pranshul.devmain@gmail.com>
2026-08-15 18:03:18 +0530

    update db

--- app/src/main/java/com/pranshulgg/weather_master_app/data/local/entity/weather/HourlyWeatherEntity.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/data/local/entity/weather/HourlyWeatherEntity.kt
@@ -1,8 +1,10 @@
 package com.pranshulgg.weather_master_app.data.local.entity.weather
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
+import com.pranshulgg.weather_master_app.core.model.sources.WeatherSource
 import com.pranshulgg.weather_master_app.core.model.weather.WeatherCondition
 import com.pranshulgg.weather_master_app.core.model.weather.wind.WindDirection
 import com.pranshulgg.weather_master_app.data.local.entity.location.WeatherLocationEntity
@@ -35,5 +37,7 @@ data class HourlyWeatherEntity(
     val humidity: Double?,
     val dewPoint: Double?,
     val time: Long,
-    val precipitationProbability: Int?
+    val precipitationProbability: Int?,
+
+    val cachedSource: WeatherSource? = null
 )
