723eb741b4abd885fd626b2bc6d3b788abe03a95
Flamme Gamer <57811689+FlammeGamer@users.noreply.github.com>
2026-09-03 21:18:32 +0200

    add support for third-party gadgetbridge receivers

--- app/src/main/java/com/pranshulgg/weather_master_app/data/worker/gadgetbridge/GadgetBridge.kt
+++ app/src/main/java/com/pranshulgg/weather_master_app/data/worker/gadgetbridge/GadgetBridge.kt
@@ -1,23 +1,12 @@
 package com.pranshulgg.weather_master_app.data.worker.gadgetbridge
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 
 
 fun isGadgetbridgeInstalled(context: Context): Boolean {
-    val pm = context.packageManager
-    val standardPackage = "nodomain.freeyourgadget.gadgetbridge"
-    val nightlyPackage = "nodomain.freeyourgadget.gadgetbridge.nightly"
-
-    return try {
-        pm.getPackageInfo(standardPackage, 0)
-        true
-    } catch (e: PackageManager.NameNotFoundException) {
-        try {
-            pm.getPackageInfo(nightlyPackage, 0)
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
-    }
+    return context.packageManager
+        .queryBroadcastReceivers(Intent("nodomain.freeyourgadget.gadgetbridge.ACTION_GENERIC_WEATHER"), PackageManager.GET_RESOLVED_FILTER)
+        .isNotEmpty()
 }
